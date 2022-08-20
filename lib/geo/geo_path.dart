4f9426b554863eb14fafb930a8f9c3397954fd36
Imran Tusneem <imran.tusneem3000@gmail.com>
2022-08-20 12:25:12 -0500

    address code formating warnings... dart analyze flutter format .

--- lib/geo/geo_path.dart
+++ lib/geo/geo_path.dart
@@ -37,8 +37,7 @@ class GeoPath {
     // COMMENTED: Radius used for Approach 2 (below)
     // Radius radius = Radius.circular(cornerRadius*5);
     for (int i = 0; i <= coords.length; i++) {
-      GeoCoordinate2D p1 =
-          coords[(i + coords.length - 1) % coords.length]; // previous point
+      GeoCoordinate2D p1 = coords[(i + coords.length - 1) % coords.length]; // previous point
       GeoCoordinate2D p2 = coords[i % coords.length]; // current point
       GeoCoordinate2D p3 = coords[(i + 1) % coords.length]; // next point
 
@@ -46,13 +45,15 @@ class GeoPath {
       // Note: Handle vertical slope (ie. infinity) as a very large number
       // get slope between point 1 & 2, an 3 & 2
       double mp2p1 = p2.slopeTo(p1); // slope between point 1 and 2
-      if (mp2p1.isInfinite)
-        mp2p1 =
-            0x80000000; // set to a large finite number, since we can't operate on infinity
+      if (mp2p1.isInfinite) {
+        // set to a large finite number, since we can't operate on infinity
+        mp2p1 = 0x80000000;
+      }
       double mp2p3 = p2.slopeTo(p3); // slope between point 3 and 2
-      if (mp2p3.isInfinite)
-        mp2p3 =
-            0x80000000; // set to a large finite number, since we can't operate on infinity
+      if (mp2p3.isInfinite) {
+        // set to a large finite number, since we can't operate on infinity
+        mp2p3 = 0x80000000;
+      }
       double mp2p1xmp2p3 = (mp2p1 * mp2p3);
 
       // get angle between line P2P1 and Line P2P3 (ie. angle of P1-P2-P3)
